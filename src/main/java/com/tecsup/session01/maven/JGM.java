d5ba04e65d503f424393cee46dd180f40190ffbf
unknown <antonio.cristobal@tecsup.edu.pe>
2019-08-27 17:29:49 -0500

    new class

---
+++ src/main/java/com/tecsup/session01/maven/JGM.java
@@ -0,0 +1,13 @@
+package com.tecsup.session01.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class JGM
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
